cee1f78aae16f2c71446c6fe0fdb0d1672acc46c
a1919727 <a1919727@adelaide.edu.au>
2026-06-25 20:58:45 +0930

    fix(auth): add sooner toaster in app layout

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { Header } from "@/components/layout/header";
 import { Footer } from "@/components/layout/footer";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,6 +36,7 @@ export default function RootLayout({
           <Header />
           {children}
           <Footer />
+          <Toaster richColors position="top-center" />
         </body>
       </html>
     </ClerkProvider>
